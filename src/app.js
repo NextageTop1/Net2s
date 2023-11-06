cc2b2ef349040f40a4fc9dc80a793f93ee385210
NextageTop1 <vitorgabrielgb6@gmail.com>
2023-11-06 16:18:28 -0300

    conecxao mysql

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
+
 const app = express()
-const conexao = require("./serve.js")
-app.get("/", (req, res) => {
-    res.send("ola")
-})
+
+
+
 
 app.listen(3000,function(){console.log("funcionando")})
--- src/serve.js
+++ src/serve.js
@@ -1,7 +1,5 @@
 
-
 const mysql = require('mysql2');
-const { Connection } = require('mysql2/typings/mysql/lib/Connection');
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
@@ -17,12 +15,14 @@ connection.connect((err) => {
     }
     console.log('Conexao bem'+connection.threadld);
 });
+
 connection.query('SELECT * FROM usuario',(err,results,fields) => {
     if(err){console.error('Error consulta'+err.stack);
     return;
 }
     console.log('Resultado:',results)
 });
+
 connection.end((err) =>{
     if(err){
         console.error('Erro ao encerrar'+err.stack)
