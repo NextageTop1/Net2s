8b4860aac2cf12dba220f0cc4bc3d50d601e8879
NextageTop1 <vitorgabrielgb6@gmail.com>
2023-11-13 15:46:42 -0300

    óla

--- src/app.js
+++ src/app.js
@@ -20,11 +20,21 @@ fetch('/home')
     .then(data => {
         const rc = document.getElementById('rc')
         if (data.results.length > 0){
+            const ul = document.createElement('ul');
+            data.results.forEach(result => {
+              const li = document.createElement('li');
+              li.textContent = result.nome_da_coluna; // Substitua pelo nome da sua coluna
+              ul.appendChild(li);
+            });
+            resultsContainer.appendChild(ul);
+          } else {
+            resultsContainer.innerHTML = '<p>Nenhum resultado encontrado.</p>';
+          }
+        })
+        .catch(error => {
+          console.error('Erro ao obter resultados:', error);
+        });
             
-        } 
-
-
-})
 app.listen(3000, () => {
     console.log("Funcionando");
 });
