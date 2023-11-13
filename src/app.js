d52436755ea630cb93bcf3b906ff0f7584202e93
litllev <vitorherisonvhp@gmail.com>
2023-11-13 16:32:12 -0300

    sim

--- src/app.js
+++ src/app.js
@@ -1,40 +1,36 @@
 const express = require('express');
-const db = require('./serve');
 const app = express();
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password:'',
+    database: 'net2s',
+})
 
-app.get('/home', (req, res) => {
-    db.connection.query('SELECT * FROM usuario', (err, results, fields) => {
+connection.connect((err) => {
+    if(err){
+        console.error('Erro'+err.stack);
+
+        return;
+    }
+    console.log('Conexao bem'+connection.threadld);
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM usuario', (err, results, fields) => {
         if (err) {
             console.error('Erro na consulta:', err.stack);
             res.status(500).send('Erro');
             return;
         }
 
-        // Aqui, assumindo que você tem um arquivo HTML chamado 'index.html'
-        res.json(results);
+        res.render('index', { options: results });
     });
 });
 
-fetch('/home')
-    .then(response => response.json())
-    .then(data => {
-        const rc = document.getElementById('rc')
-        if (data.results.length > 0){
-            const ul = document.createElement('ul');
-            data.results.forEach(result => {
-              const li = document.createElement('li');
-              li.textContent = result.nome_da_coluna; // Substitua pelo nome da sua coluna
-              ul.appendChild(li);
-            });
-            resultsContainer.appendChild(ul);
-          } else {
-            resultsContainer.innerHTML = '<p>Nenhum resultado encontrado.</p>';
-          }
-        })
-        .catch(error => {
-          console.error('Erro ao obter resultados:', error);
-        });
-            
 app.listen(3000, () => {
     console.log("Funcionando");
 });
