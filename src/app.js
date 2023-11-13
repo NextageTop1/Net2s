ef9b4c358705f55f70f8bdd811d2c25d428fd993
litllev <vitorherisonvhp@gmail.com>
2023-11-13 15:42:40 -0300

    muita coisa

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
-const express = require('express')
+const express = require('express');
+const db = require('./serve');
+const app = express();
 
-const app = express()
+app.get('/home', (req, res) => {
+    db.connection.query('SELECT * FROM usuario', (err, results, fields) => {
+        if (err) {
+            console.error('Erro na consulta:', err.stack);
+            res.status(500).send('Erro');
+            return;
+        }
+
+        // Aqui, assumindo que você tem um arquivo HTML chamado 'index.html'
+        res.json(results);
+    });
+});
+
+fetch('/home')
+    .then(response => response.json())
+    .then(data => {
+        const rc = document.getElementById('rc')
+        if (data.results.length > 0){
+            
+        } 
 
 
-
-
-app.listen(3000,function(){console.log("funcionando")})
+})
+app.listen(3000, () => {
+    console.log("Funcionando");
+});
--- src/serve.js
+++ src/serve.js
@@ -15,8 +15,23 @@ connection.connect((err) => {
     }
     console.log('Conexao bem'+connection.threadld);
 });
+/* const usu = {
+    nome: 'victor',
+    telefone: 12345677,
+    senha:'werwr ',
+    email: 'exemple@email',
+    permissao: 1,
+    endCobranca: 'rwerw ',
+    endEntrega: ' rwerwer'
+}
+const query = 'INSERT INTO usuario ( `usu_nome`, `telefone`, `senha`, `usu_email`, `usu_permissao`, `usu_end_cobranca`, `usu_end_entrega`) VALUES (?,?,?,?,?,?,?)'
+connection.query(query,[usu.nome,usu.telefone,usu.senha,usu.email,usu.permissao,usu.endCobranca,usu.endEntrega],(err, results)=> {
+    if (err) {
+        console.error('Erro ao fechar a conexão:', err);
+        return;
+}}) */
 
-connection.query('SELECT * FROM usuario',(err,results,fields) => {
+/* connection.query('SELECT * FROM usuario',(err,results,fields) => {
     if(err){console.error('Error consulta'+err.stack);
     return;
 }
@@ -27,4 +42,6 @@ connection.end((err) =>{
     if(err){
         console.error('Erro ao encerrar'+err.stack)
     }
-});
+}); */
+
+module.exports = {connection}
