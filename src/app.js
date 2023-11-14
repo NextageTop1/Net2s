1866d1b388b482d0331d42df7c2882b39c17f339
NextageTop1 <vitorgabrielgb6@gmail.com>
2023-11-13 23:16:35 -0300

    ta bom

--- src/app.js
+++ src/app.js
@@ -14,3 +14,53 @@ const handlebars = require('express-handlebars');
 app.listen(3000, () => {
   console.log('Funcionando');
 });
+
+// const express = require('express');
+// const app = express();
+// const mysql = require('mysql2');
+// const path = require('path');  // Adicione esta linha
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'net2s',
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Erro' + err.stack);
+//         return;
+//     }
+//     console.log('Conexao bem' + connection.threadId);
+// });
+
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));  // Corrigido: Importe o módulo path
+
+// app.get('/21', (req, res) => {
+//     connection.query('SELECT * FROM usuario', (err, results, fields) => {
+//         if (err) {
+//             console.error('Erro na consulta:', err.stack);
+//             res.status(500).send('Erro');
+//             return;
+//         }
+
+//         res.send(results);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//     connection.query('SELECT * FROM usuario', (err, results, fields) => {
+//         if (err) {
+//             console.error('Erro na consulta:', err.stack);
+//             res.status(500).send('Erro');
+//             return;
+//         }
+//         res.render('index', { options: results });
+//     });
+// });
+
+// app.listen(3000, () => {
+//     console.log("Funcionando");
+// });
